9dd8ea626666109b953ed93711ec927a728ea15d
Kornenko Kirill <kornenko_k@mail.ru>
2025-03-31 03:37:49 +0300

    Подключил к проекту ejs. Сделал 2 шаблона. Добавил чтобы имя пользователя доставалось из тела запроса и выдавалось приветствие на странице. Подключён файл css.

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,14 @@ const port = 3000
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(express.static('public'));
+
+app.set("view engine","ejs")
+
+app.get('/ejs-pages', (req: Request, res: Response) => {
+    const user = req.query.user || 'anonymous';
+    res.render('layout', {message: "My message123", title: "My page",content: "index", user})
+})
 
 app.post('/', (req: Request, res: Response) => {
     console.log(req.body)
